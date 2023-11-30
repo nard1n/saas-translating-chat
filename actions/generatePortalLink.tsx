485f3c8606c5c45933ec9ae052aa94bd341feb73
Nardin <nlachowski@nlachow-ltmy3wc.internal.salesforce.com>
2023-11-30 10:49:11 -0500

    correct api version

--- actions/generatePortalLink.tsx
+++ actions/generatePortalLink.tsx
@@ -8,8 +8,8 @@ import { redirect } from "next/navigation";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2023-10-16",
-})
+    apiVersion: "2023-08-16",
+  });
 
 export async function generatePortalLink() {
     const session = await getServerSession(authOptions);
